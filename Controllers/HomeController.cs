0e0b92c249934b829c55e637d3e9ef99465c92ab
Quentin Ng <qng1997@gmail.com>
2019-08-08 19:02:09 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SurveyWithModel.Models;
+
+namespace SurveyWithModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        //for each route this controller is to handle:
+        [HttpGet]       //type of request
+        [Route("")]     //associated route string (exclude the leading /)
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+        [HttpPost("submit")]
+        public IActionResult Submit(Survey Submission){
+            return View("Result",Submission);
+        }
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace SurveyWithModel.Models{
+    public class Survey{
+        public string name{get;set;}
+        public string location{get;set;}
+        public string language{get;set;}
+        public string comments{get;set;}
+
+    }
+}
